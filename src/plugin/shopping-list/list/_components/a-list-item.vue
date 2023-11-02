acc51d58c8072ea474f27d4966baa5304c91d869
MarchevskySimon <jety258@gmail.com>
2023-11-02 19:27:17 +0100

    Uz funguje databaza a vypis

--- src/plugin/shopping-list/list/_components/a-list-item.vue
+++ src/plugin/shopping-list/list/_components/a-list-item.vue
@@ -1,39 +1,70 @@
 <template>
   <div>
-    <!-- <section v-for="list in listData" :key="list.id">
+    <section v-for="list in shoppingLists" :key="list.id">
       <RouterLink :to="`/list/${list.id}`">{{ list.title }}</RouterLink>
-    </section> -->
-    <!-- <p>{{ listData }}</p> -->
+    </section>
+
+    <h1>Lists</h1>
+
+    <template v-if="!shoppingLists">
+      <p>Načítavam dáta</p>
+    </template>
+
+    <template v-else-if="shoppingLists.error">
+      Pri načítaní dát nastala chyba: {{ shoppingLists.error }}
+    </template>
+
+    <template v-else>
+      Počet položiek v zozname: {{ shoppingLists.length }}
+    </template>
   </div>
 </template>
 
 <script>
-import { ref } from "vue";
+// import { ref } from "vue";
 // import { data } from "@/data.js";
 import axios from "axios";
 
 export default {
-  setup() {
-    const listData = ref("");
-
-    axios
-      .get("https://shoppinglist.wezeo.dev/shoppinglist/lists")
-      .then((response) => {
-        listData.value = response;
-      });
-    console.log(listData);
-
+  data() {
     return {
-      // listData,
+      shoppingLists: null,
     };
   },
-  // setup() {
-  //   const listData = ref(data);
-  //   return {
-  //     listData,
-  //   };
-  // },
+
+  async mounted() {
+    try {
+      // const response = await axios.get('https://shoppinglist.wezeo.dev/shoppinglist/lists')
+      // const data = response.data.data
+
+      // Skrateny zapis zakomentovaneho kodu vyssie, kde vytiahneme data pomocou destrukcie objektu
+      const {
+        data: { data: shoppingLists },
+      } = await axios.get("/api/v1/shopping-lists");
+      this.shoppingLists = shoppingLists;
+    } catch (error) {
+      console.error("Error:", error);
+      this.shoppingLists = { error };
+    }
+  },
 };
+
+// export default {
+//   setup() {
+//     const listData = ref("");
+
+//     axios
+//       .get("https://shoppinglist.wezeo.dev/shoppinglist/lists")
+//       .then((response) => {
+//         listData.value = response;
+//       });
+//     console.log(listData);
+
+//     return {
+//       listData,
+//     };
+//   },
+// };
 </script>
 
 <style lang="scss" scoped></style>
