f1ea2b2e2aa1a57b07e49b8323fbef700dddee28
MarchevskySimon <jety258@gmail.com>
2023-11-06 21:10:55 +0100

    Pridal som axios.post()

--- src/plugin/shopping-list/list/the-lists.vue
+++ src/plugin/shopping-list/list/the-lists.vue
@@ -1,24 +1,35 @@
 <template>
   <main>
     <h1>LISTS</h1>
-    <form @submit.prevent="">
-      <input type="text" />
+    <form @submit.prevent="addToDatabase()">
+      <input type="text" v-model="titleName" />
       <button type="submit">ADD NEW</button>
     </form>
-
     <ListItem />
   </main>
 </template>
 
 <script>
 import ListItem from "@/plugin/shopping-list/list/_components/a-list-item.vue";
+import axios from "axios";
 
 export default {
   components: {
     ListItem,
   },
   data() {
-    return {};
+    return {
+      titleName: "",
+    };
+  },
+  methods: {
+    addToDatabase() {
+      axios
+        .post("/api/v1/shopping-lists", { title: this.titleName })
+        .then(function respose(e) {
+          console.log(e);
+        });
+    },
   },
 };
 </script>
